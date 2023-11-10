08a7d073d527167dfc28bf1f7371e5189c7c5246
vermavishal12 <149952920+vermavishal12@users.noreply.github.com>
2023-11-10 19:39:35 +0530

    Update 1866-restore-the-array-from-adjacent-pairs.cpp (Optimised solution))

--- 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
@@ -2,42 +2,40 @@ class Solution {
 public:
 
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-        unordered_map<int,vector<int>>map;
+        unordered_map<int,vector<int>>map;        // map to store the adjacency list.
         int n = adjacentPairs.size();
         
-        for(auto pairs : adjacentPairs) {
+        for(auto& pairs : adjacentPairs) {
             map[pairs[0]].push_back(pairs[1]);
             map[pairs[1]].push_back(pairs[0]);
         }
 
-        unordered_map<int,bool>M;
-        vector<int>res;
-        
-        for(auto m : map) {
+        vector<int>res(n+1);        // resultant array to be returned.
+        int curr, prev, i=2;        // curr => current array that we are processing, prev => adjacent of the current element in the resultant array.
+
+        for(auto &m : map) {
             if(m.second.size() == 1) {
-                res.push_back(m.first);  
-                M[m.first] = true;      
+                prev = m.first;
+                curr = m.second[0];
                 break;
             }
         }
-        int N = 1;
-        
-        while(N != n+1) {
-            for(auto val : map[res[N-1]]) {
-                if(!M[val]) {
-                    M[val] = true;
-                    res.push_back(val);
-                    N++;
-                }
+        res[0] = prev;
+        res[1] = curr;
+        while(map[curr].size() != 1) {        // loop to iterate over each element and build the resultant array.
+            if(map[curr][0] == prev) {
+                res[i] = map[curr][1];
             }
+            else{
+                res[i] = map[curr][0];
+            }
+            prev = curr;
+            curr = res[i];
+            i++;
         }
 
+        res[n] = curr;
+
         return res;
-        
-        
-
-        return {};
-
     }
 };
-
