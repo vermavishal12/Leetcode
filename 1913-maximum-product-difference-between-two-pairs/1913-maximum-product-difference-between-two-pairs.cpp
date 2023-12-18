519783a8ec4dc3fdcd5f6765295a7b89c2eb29b7
vermavishal12 <149952920+vermavishal12@users.noreply.github.com>
2023-12-18 20:19:22 +0530

    Time: 20 ms (74.89%), Space: 20.6 MB (64.73%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -3,27 +3,25 @@ public:
     int maxProductDifference(vector<int>& nums) {
         int minA = INT_MAX, minB = INT_MAX, maxA = INT_MIN, maxB = INT_MIN, n = nums.size();
         
-        for(int i = 0; i < n ; i++) {
-            
-            if(minA >= nums[i]) {
+        for(auto &num : nums) {
+            if(minA >= num) {
                 minB = minA;
-                minA = nums[i];
+                minA = num;
             }
-            else if(minB >= nums[i]) {
-                minB= nums[i];
+            else if(minB >= num) {
+                minB= num;
             }
             
-            if(maxA <= nums[i]) {
+            if(maxA <= num) {
                 maxB = maxA;
-                maxA = nums[i];
-            } else if(maxB <= nums[i]) {
-                maxB = nums[i];
+                maxA = num;
+            } else if(maxB <= num) {
+                maxB = num;
             }
         
                 
         }
-        // cout<<minA<<" "<<minB;
-        // return 0;
+        
         return ((maxB*maxA) - (minA*minB));
     }
 };
