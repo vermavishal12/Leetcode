0fd1e75db4ace60a91f72616eb71cf60730054ec
Vishal Verma <vermavishal9119196@gmail.com>
2024-07-04 23:38:56 +0530

    Added the submission

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution{
+    public: 
+        ListNode* mergeNodes(ListNode* head) {
+            int curr = 0;
+
+            
+            ListNode* newHead = new ListNode(-1);
+            ListNode* itr = head, *ptr = newHead;
+            while(!itr) {
+                if(itr->val == 0) {
+                    if(curr != 0) {
+                        ListNode* newNode = new ListNode(curr);
+                        ptr->next = newNode;
+                        ptr = newNode;
+                    }
+                }else{
+                    curr += itr->val;
+                }
+            }
+        }
+
+        return newHead->next;
+}
