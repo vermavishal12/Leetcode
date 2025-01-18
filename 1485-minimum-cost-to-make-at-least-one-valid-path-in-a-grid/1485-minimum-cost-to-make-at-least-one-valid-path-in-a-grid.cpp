420ce9d8195cb576276e0b10dc1d7ea2e6fa5936
Vishal Verma <149952920+vermavishal12@users.noreply.github.com>
2025-01-18 15:11:26 +0530

    Time: 34 ms (49.51%), Space: 18.5 MB (76.79%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -27,14 +27,6 @@ public:
                 }
             }
         }
-
-        for(int i = 0; i < m ; i++) {
-            for(int j = 0; j < n ; j++) {
-                cout<<costArray[i][j] <<" ";
-            }
-            cout<<endl;
-        }
-
         return costArray[m-1][n-1];
     }
 };
